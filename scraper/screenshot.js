576b6b7646503be2f906857b7f59df5f912037fb
wimdavies <william.johnston.davies@gmail.com>
2023-09-18 17:05:01 +0100

    Add root ESLint config

--- scraper/screenshot.js
+++ scraper/screenshot.js
@@ -1,9 +1,9 @@
-const puppeteer = require('puppeteer');
+import { launch } from 'puppeteer';
 
 (async () => {
- const browser = await puppeteer.launch({headless: 'new'});
- const page = await browser.newPage();
- await page.goto('https://www.rspb.org.uk/birds-and-wildlife/wildlife-guides/identify-a-bird/');
- await page.screenshot({path: 'screenshot.png'});
-await browser.close();
+  const browser = await launch({ headless: 'new' });
+  const page = await browser.newPage();
+  await page.goto('https://www.rspb.org.uk/birds-and-wildlife/wildlife-guides/identify-a-bird/');
+  await page.screenshot({ path: 'screenshot.png' });
+  await browser.close();
 })();
