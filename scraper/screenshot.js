fb63bf3cc4b1736bd7d3d324646659078604ea40
wimdavies <william.johnston.davies@gmail.com>
2023-09-19 16:19:19 +0100

    Fix ESM conflict with linter; run screenshot.js in headful mode

--- scraper/screenshot.js
+++ scraper/screenshot.js
@@ -1,7 +1,7 @@
 import { launch } from 'puppeteer';
 
 (async () => {
-  const browser = await launch({ headless: 'new' });
+  const browser = await launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://www.rspb.org.uk/birds-and-wildlife/wildlife-guides/identify-a-bird/');
   await page.screenshot({ path: 'screenshot.png' });
