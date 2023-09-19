88b3554cf78ee97f5e52ca1ab7612427e742497f
wimdavies <william.johnston.davies@gmail.com>
2023-09-19 20:46:07 +0100

    Remove root ESLint, add ESLint for /scraper. Reorganise screenshot output into named directory (gitignored)

--- scraper/screenshot.js
+++ scraper/screenshot.js
@@ -4,6 +4,6 @@ import { launch } from 'puppeteer';
   const browser = await launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://www.rspb.org.uk/birds-and-wildlife/wildlife-guides/identify-a-bird/');
-  await page.screenshot({ path: 'screenshot.png' });
+  await page.screenshot({ path: 'scraper/screenshots/landing_screenshot.png' });
   await browser.close();
 })();
