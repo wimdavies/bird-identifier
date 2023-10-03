ea36a59a06734a2cea31d6d3bbaa71a5c01d1909
wimdavies <william.johnston.davies@gmail.com>
2023-10-03 12:53:19 +0100

    Create importable array of size filter selectors in /utils

---
+++ scraper/utils/sizeFilterSelectors.js
@@ -0,0 +1,13 @@
+const sizeFilterSelectors = [
+  '#panel-size > div.bird-identifier__options__submenu__container > li:nth-child(3) > label',
+  '#panel-size > div.bird-identifier__options__submenu__container > li:nth-child(4) > label',
+  '#panel-size > div.bird-identifier__options__submenu__container > li:nth-child(5) > label',
+  '#panel-size > div.bird-identifier__options__submenu__container > li:nth-child(6) > label',
+  '#panel-size > div.bird-identifier__options__submenu__container > li:nth-child(7) > label',
+  '#panel-size > div.bird-identifier__options__submenu__container > li:nth-child(8) > label',
+  '#panel-size > div.bird-identifier__options__submenu__container > li:nth-child(9) > label',
+  '#panel-size > div.bird-identifier__options__submenu__container > li:nth-child(10) > label',
+  '#panel-size > div.bird-identifier__options__submenu__container > li:nth-child(11) > label',
+];
+
+export default sizeFilterSelectors;
