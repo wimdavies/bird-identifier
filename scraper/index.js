fdc0189c4d5a69ba405eb437c88874319a96a371
wimdavies <william.johnston.davies@gmail.com>
2023-10-09 13:43:50 +0100

    Build towards script that accommodates inconsistent behaviour on results loading

--- scraper/index.js
+++ scraper/index.js
@@ -8,7 +8,8 @@ import filterScraper from './utils/filterScraper.js';
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
-    slowMo: 100,
+    // slowMo option slows all operations down to assist debugging
+    slowMo: 50,
     args: [
       '--disable-infobars',
       '--start-maximized',
@@ -41,11 +42,33 @@ import filterScraper from './utils/filterScraper.js';
     await page.$eval(applyButtonSelector, (element) => element.click());
     console.log('clicked "apply"');
 
+    // Control flow to rescue inconsistent behaviour
+    // Now realised this arises from issue with website, not scraper
+    // TODO: replace this with error handling involving a page reload
+    const resultsPage1ButtonSelector = '#main-content > div > div:nth-child(2) > div > div.bird-identifier > div.bird-identifier__panel > form > div:nth-child(2) > div > div > div > div.pagination--compact > div.pagination__numbers > a:nth-child(1)';
+    const isResultsPage1ButtonSelectorActive = await page.$('#main-content > div > div:nth-child(2) > div > div.bird-identifier > div.bird-identifier__panel > form > div:nth-child(2) > div > div > div > div.pagination--compact > div.pagination__numbers > a.pagination__link.active');
+    const firstImgSrcSelector = '#main-content > div > div:nth-child(2) > div > div.bird-identifier > div.bird-identifier__panel > form > div:nth-child(2) > div > div > div > div.row > a > div > figure > picture > img';
+    const isFirstImgSrcSelector = await page.$(firstImgSrcSelector);
+
+    if (!isResultsPage1ButtonSelectorActive && !isFirstImgSrcSelector) {
+      console.log('The results have NOT loaded in the expected way');
+      await page.waitForSelector(resultsPage1ButtonSelector);
+      console.log('waited for results page 1 button to load');
+      await page.$eval(resultsPage1ButtonSelector, (element) => element.click());
+      console.log('clicked results page 1 button');
+      await page.waitForSelector(firstImgSrcSelector);
+      console.log('waited for first result image to load');
+    } else {
+      console.log('The results have loaded in the expected way');
+      await page.waitForSelector(firstImgSrcSelector);
+      console.log('waited for first result image to load');
+    }
+
     const filterResults = await filterScraper(page);
 
     scrapedBirds.push(...filterResults);
-    console.log('Scraped birds so far:');
-    console.log(scrapedBirds);
+    // console.log('Scraped birds so far:');
+    // console.log(scrapedBirds);
 
     const clearAllOptionsSelector = '#main-content > div > div:nth-child(2) > div > div.bird-identifier > div.bird-identifier__panel > form > div:nth-child(2) > ul > div > button.bird-identifier__fixed-bottom__current-indicator__item.clear';
     await page.waitForSelector(clearAllOptionsSelector);
--- scraper/utils/filterScraper.js
+++ scraper/utils/filterScraper.js
@@ -3,25 +3,6 @@ export default async function filterScraper(page) {
   let counter = 1;
 
   while (true) {
-    const resultsPage1ButtonSelector = '#main-content > div > div:nth-child(2) > div > div.bird-identifier > div.bird-identifier__panel > form > div:nth-child(2) > div > div > div > div.pagination--compact > div.pagination__numbers > a:nth-child(1)';
-    const isResultsPage1ButtonSelector = await page.$(resultsPage1ButtonSelector);
-    const firstImgSrcSelector = '#main-content > div > div:nth-child(2) > div > div.bird-identifier > div.bird-identifier__panel > form > div:nth-child(2) > div > div > div > div.row > a > div > figure > picture > img';
-    const isFirstImgSrcSelector = await page.$(firstImgSrcSelector);
-
-    if (isResultsPage1ButtonSelector && !isFirstImgSrcSelector) {
-      console.log('The results have NOT loaded in the expected way');
-      await page.waitForSelector(resultsPage1ButtonSelector);
-      console.log('waited for results page 1 button to load');
-      await page.$eval(resultsPage1ButtonSelector, (element) => element.click());
-      console.log('clicked results page 1 button');
-      await page.waitForSelector(firstImgSrcSelector);
-      console.log('waited for first result image to load');
-    } else {
-      console.log('The results have loaded in the expected way');
-      await page.waitForSelector(firstImgSrcSelector);
-      console.log('waited for first result image to load');
-    }
-
     const pageResults = await page.$$eval(
       '#main-content > div > div:nth-child(2) > div > div.bird-identifier > div.bird-identifier__panel > form > div:nth-child(2) > div > div > div > div.row > a',
       (elements) => elements.map((element) => ({
@@ -48,8 +29,8 @@ export default async function filterScraper(page) {
     counter += 1;
   }
 
-  console.log('Filter results:');
-  console.log(filterResults);
+  // console.log('Filter results:');
+  // console.log(filterResults);
 
   return filterResults;
 }
